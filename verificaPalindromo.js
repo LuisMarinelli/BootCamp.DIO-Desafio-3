90cad2a0f7748bb480dc69b79d0e8eab779a161d
Luis Marinelli <luis_marinelli@hotmail.com>
2022-06-29 10:39:24 -0300

    Solução 2, com laço de 'For'
    Solução 2, com laço de 'For', onde a verificação ocorre do inicio até a metade da string enquanto paralelamente ocorre a comparação do final até a metade da string.

--- verificaPalindromo.js
+++ verificaPalindromo.js
@@ -1,10 +1,25 @@
+//Solução 1
 //Verifica Palíndromo simples (sem espaços ou acentuações)
-function verificar(string){
+function verificar(string) {
     //verifica existencia de um texto - não precisa do Else
-    if(!string) return;
+    if (!string) return "Não existe nenhuma String";
 
     //separa os textos, faz um reverso e junta com o método Join para comparar
     return string.split("").reverse().join("") === string;
 }
 
-console.log(verificar("amor"));
+//Solução 2
+function verificar2(string) {
+    //verifica existencia de um texto - não precisa do Else
+    if (!string) return "Não existe nenhuma String.";
+
+    for(let i = 0; i < string.length / 2; i++){
+        if(string[i] !== string[string.length -1 -i]){
+            return false;
+        }
+    }
+
+    return true;
+}
+
+console.log(verificar2("amo"));
